04534ab6437360943451b8418ffe95e0ff3c564b
harshadparelkar <harshad.parelkar84@gmail.com>
2015-07-16 16:22:23 +0530

    setup standalone hibernate

---
+++ src/main/java/com/test/git/mvn/hibernateTest/entity/EmployeeDetails.java
@@ -0,0 +1,46 @@
+package com.test.git.mvn.hibernateTest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class EmployeeDetails {
+
+	String 	id,
+			address,
+			phone,
+			city;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Id
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
